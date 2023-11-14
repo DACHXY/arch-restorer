30a18db54d4401e469b4f8172e5e7893d34cfcdb
DACHXY <Danny10132024@gmail.com>
2023-11-14 08:34:59 +0000

    Change structure

--- run.sh
+++ run.sh
@@ -4,9 +4,84 @@ export ZSH_SCRIPTS="./src/zshconfig/scripts"
 export CONFIG_PATH="$HOME/.config"
 export DOTFILE_PATH="./src/dotfiles"
 
-mkdir -p "$CONFIG_PATH/i3"
-mkdir -p "$CONFIG_PATH/kitty"
 mkdir -p "$ZSH_SCRIPTS"
+
+pacman_packages=(
+    "base-devel"
+    "gnupg"
+    "grub"
+    "iw"
+    "wpa_supplicant"
+    "wireless_tools"
+    "networkmanager"
+    "network-manager-applet"
+    "ibus"
+    "gnome-keyring"
+    "zsh"
+    "dialog"
+    "intel-ucode"
+    "reflector"
+    "lshw"
+    "htop"
+    "alsa-utils"
+    "alsa-plugins" 
+    "pavucontrol" 
+    "xdg-user-dirs" 
+    "pulseaudio" 
+    "bluez"
+    "bluez-utils"
+    "blueman"
+    "tlp"
+    "tlp-rdw"
+    "powertop"
+    "acpi"
+    "curl"
+    "unzip"
+    "nano"
+    "git"
+    "wget"
+    "exa"
+    "feh"
+    "xorg-server"
+    "xorg-apps"
+    "xorg-xinit"
+    "i3"
+    "i3-gaps"
+    "i3blocks"
+    "i3lock"
+    "numlockx"
+    "arduino-cli"
+    "lightdm"
+    "lightdm-webkit2-greeter"
+    "noto-fonts"
+    "ttf-ubuntu-font-family"
+    "ttf-dejavu"
+    "ttf-freefont"
+    "ttf-liberation"
+    "ttf-droid"
+    "ttf-roboto"
+    "terminus-font"
+    "ttf-cascadia-code-nerd"
+    "kitty"
+    "ranger"
+    "rofi"
+    "dmenu"
+    "xss-lock"
+    "polybar"
+    "firefox"
+    "vlc"
+    "opera"
+    "docker"
+    "docker-compose"
+    "docker-buildx"
+)
+
+aur_packages=(
+    "picom-allusive"
+    "lightdm-webkit2-theme-glorious"
+    "docker-desktop"
+    "snapd"
+)
 
 # pacman install with no-confirm & needed
 pacman_install() {
@@ -14,36 +89,37 @@ pacman_install() {
 }
 
 paru_install() {
-    paru -Sq --needed --noconfirm "$@" 
+    paru -Sq --sudoloop --needed --noconfirm "$@" 
 }
 
-InstallDependencies() {
-    pacman_install base-devel
+ConfigSystem() {
+    # Let user control wireless system
+    sudo systemctl mask systemd-rfkill.service
+    sudo systemctl mask systemd-rfkill.socket
 
-    # Grub Tool
-    pacman_install grub 
+    # Enable SSD TRIM
+    sudo systemctl enable fstrim.timer
 
-    # Network Tool
-    pacman_install iw wpa_supplicant wireless_tools networkmanager network-manager-applet 
-
-    # System Tool
-    pacman_install ibus gnome-keyring dialog intel-ucode reflector lshw htop alsa-utils alsa-plugins pavucontrol xdg-user-dirs pulseaudio 
+    # Enable Network Manager
+    sudo systemctl enable NetworkManager
 
     # Bluetooh
-    pacman_install bluez bluez-utils blueman 
     sudo systemctl enable bluetooth
 
     # Power
-    pacman_install tlp tlp-rdw powertop acpi 
     sudo systemctl enable tlp
-}
 
-InstallTools() {
-    # Tools
-    pacman_install curl unzip nano git wget exa
-    
-    # wallpaper manager
-    pacman_install feh
+    # Display Manager
+    sudo systemctl enable lightdm
+
+    # Openssh Server
+    sudo systemctl enable sshd
+
+    # Enable docker
+    sudo systemctl enable --now docker.service
+
+    # Enable docker-desktop
+    systemctl --user enable docker-desktop
 }
 
 InstallParu() {
@@ -74,48 +150,11 @@ InstallAntigen() {
     sudo chmod 777 "$ZSH_SCRIPTS/antigen.zsh"
 }
 
-Installi3wm() {
-    # Install i3 dependency
-    pacman_install xorg-server xorg-apps xorg-xinit
-    pacman_install i3 i3-gaps i3blocks i3lock numlockx
-}
-
-ConfigSystem() {
-    # Let user control wireless system
-    sudo systemctl mask systemd-rfkill.service
-    sudo systemctl mask systemd-rfkill.socket
-
-    # Enable SSD TRIM
-    sudo systemctl enable fstrim.timer
-
-    # Enable Network Manager
-    sudo systemctl enable NetworkManager
-}
-
 ConfigZSH() {
     cp "src/zshrc" "$HOME/.zshrc"
     cp -r "src/zshconfig" "$HOME/.zshconfig" 
     sudo chmod -R +x "$HOME/.zshconfig"
     sudo chmod +x "$HOME/.zshrc"
-}
-
-ConfigKitty() {
-    if [ ! -d "$CONFIG_PATH/kitty/" ]; then
-        mkdir -p "$CONFIG_PATH/kitty/"
-    fi
-    cp -f "$DOTFILE_PATH/kitty.conf" "$CONFIG_PATH/kitty/kitty.conf"
-    cp -f "$DOTFILE_PATH/mocha.conf" "$CONFIG_PATH/kitty/mocha.conf"
-}
-
-ConfigPicom() {
-    cp -f "$DOTFILE_PATH/picom.conf" "$CONFIG_PATH/picom.conf"
-}
-
-Configi3wm(){
-    des="$CONFIG_PATH/i3/config"
-    src="$DOTFILE_PATH/i3config"
-    sudo chmod 777 "$src"
-    cp -f "$src" "$des"
 }
 
 ConfigWallpaperManager(){
@@ -129,34 +168,26 @@ ConfigWallpaperManager(){
 ConfigLightDM() {
     sudo cp "./src/user.face" "/usr/share/icons/user.face"
     sudo sed -i 's/^#greeter-session=.*/greeter-session=lightdm-webkit2-greeter/' /etc/lightdm/lightdm.conf
-    sudo cp -f "$DOTFILE_PATH/lightdm-webkit2-greeter.conf"  /etc/lightdm/lightdm-webkit2-greeter.conf
+    sudo cp -f "./src/lightdm-webkit2-greeter.conf"  /etc/lightdm/lightdm-webkit2-greeter.conf
     sudo chmod 644 /etc/lightdm/lightdm-webkit2-greeter.conf
 }
 
-ConfigGit() {
+ConfigUser() {
     git config --global user.name DACHXY
     git config --global user.email Danny10132024@gmail.com
-}
-
-ConfigVScode() {
-    cp -f $DOTFILE_PATH/vscode/* "$HOME/.vscode"
-}
-
-ConfigUser() {
     sudo usrmod -aG tty "$USER"
+    sudo chmod a+rw /dev/ttyACM0
+}
+
+CopyDotfiles() {
+    cp -f ./src/dotfiles/* "$CONFIG_PATH"
 }
 
 # === Main === #
 sudo -v
 sudo pacman -Syyu --needed --noconfirm
 
-InstallDependencies
-InstallTools
-ConfigGit
-ConfigUser
-
-# Install i3 window manager
-Installi3wm
+pacman_install "${pacman_packages[@]}"
 
 # Install paru
 if command -v paru &> /dev/null ; then
@@ -166,44 +197,12 @@ else
     InstallParu
 fi
 
-# Config System setting
-ConfigSystem
+paru_install "${aur_packages[@]}"
 
-# Install Compositor
-paru_install picom-allusive --sudoloop
-
-# Install display Manager
-pacman_install lightdm lightdm-webkit2-greeter
-paru_install lightdm-webkit2-theme-glorious --sudoloop
-sudo systemctl enable lightdm
-
-# Install some basic fonts
-pacman_install noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont
-pacman_install ttf-liberation ttf-droid ttf-roboto terminus-font
-
-# Install nerd font
-pacman_install ttf-cascadia-code-nerd
-
-# Install tools on i3
-pacman_install kitty ranger rofi dmenu xss-lock
-
-# Install some GUI programs 
-pacman_install firefox vlc opera
-
-# Install other tools
-paru_install docker-desktop
-
-# Install snapd
-paru_install snapd --sudoloop
-
+# Config snap class path
 sudo ln -s /var/lib/snapd/snap /snap
-
-# Install Official vs code
+# Install Official vscode
 sudo snap install code --classic
-ConfigVScode
-
-# Install ZSH & oh my zsh
-pacman_install zsh
 
 # Install Oh My ZSH
 OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
@@ -220,17 +219,15 @@ InstallZJumper
 # Install Antigen (zsh plugin install tool)
 InstallAntigen
 
-# Config zshrc
+# Config Section
+ConfigSystem
+ConfigUser
 ConfigZSH
-
-# Config Some tools
 ConfigWallpaperManager
-ConfigKitty
-ConfigPicom
 ConfigLightDM
 
-# Config i3 wm
-Configi3wm
+# Copy Dotfiles
+CopyDotfiles
 
 # go into zsh
 zsh
